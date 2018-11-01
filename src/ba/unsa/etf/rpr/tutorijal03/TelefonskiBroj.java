430de6600f4da5899fcce7f3178256bb47dcaba8
kkadusic <kkadusic@etf.unsa.ba>
2018-11-01 10:13:05 +0100

    Zavrsio klasu TelefonskiBroj

--- src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -2,10 +2,13 @@ package ba.unsa.etf.rpr.tutorijal03;
 
 public abstract class TelefonskiBroj implements Comparable<TelefonskiBroj> {
     public abstract String ispisi();
+
     @Override
     public abstract int hashCode();
+
     @Override
     public abstract boolean equals(Object o);
+
     public int compareTo (TelefonskiBroj t){
         return this.ispisi().compareTo(t.ispisi());
     }
