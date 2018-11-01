a36b11a15d341474ea330ea507f72f2860abd2b8
kkadusic <kkadusic@etf.unsa.ba>
2018-11-01 10:28:36 +0100

    Dodao switch-case opciju

--- src/ba/unsa/etf/rpr/tutorijal03/Program.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Program.java
@@ -11,7 +11,6 @@ public class Program {
         MobilniBroj mobilni = new MobilniBroj(n, s);
         return mobilni;
     }
-
     public MedunarodniBroj unesiMedunarodni() {
         System.out.println("Unesite broj:\n");
         Scanner ulaz = new Scanner(System.in);
@@ -21,4 +20,25 @@ public class Program {
         MedunarodniBroj medunarodni = new MedunarodniBroj(s2, s1);
         return medunarodni;
     }
+    public static void main(String[] args) {
+        int izbor = -1;
+        Imenik imenik = new Imenik();
+        System.out.println("Izaberite opciju (-1 za kraj): \n" +
+                "1. Unesite mobilni broj: \n" +
+                "2. Unesite medunarodni broj: \n" +
+                "3. Unesite fiksni broj: \n" +
+        );
+        switch(izbor) {
+            case -1:
+                System.exit(0);
+            case 1:
+                MobilniBroj mobilni = new MobilniBroj(unesiMobilni());
+                System.out.println("Unijeli ste broj: " + mobilni.ispisi() = "\n");
+                break;
+            case 2:
+                MedunarodniBroj medunarodni = new MedunarodniBroj(unesiMedunarodni());
+                System.out.println("Unijeli ste broj: " + medunarodni.ispisi() = "\n");
+                break;
+
+        }
 }
