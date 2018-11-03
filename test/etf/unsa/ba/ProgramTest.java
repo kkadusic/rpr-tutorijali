0dae18b38f15719d4c13cf56620fd86ff0ed115c
Kerim Kadušić <44180058+kkadusic@users.noreply.github.com>
2018-11-03 10:59:28 +0100

    Dodao dva testa

--- test/etf/unsa/ba/ProgramTest.java
+++ test/etf/unsa/ba/ProgramTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProgramTest {
-
     @Test
     void PostojiPredmet() {
         Program p = new Program();
@@ -20,5 +19,20 @@ class ProgramTest {
         p.DodajStudenta("Jerry", "Tom", 20000);
         assertEquals(true, p.PostojiStudent(20000));
     }
-
+    
+    @Test
+    void imaStudent() {
+        Program p = new Program();
+        assertEquals(false, p.ImaStudent(10000));
+        p.DodajStudenta("Tony", "Stark", 10000);
+        assertEquals(true, p.ImaStudent(10000));
+    }
+    
+    @Test
+    void imaPredmet() {
+        Program p = new Program();
+        assertEquals(false, p.ImaPredmet("KKK"));
+        p.KreirajPredmet("KKK", 123, 50);
+        assertEquals(true, p.ImaPredmet("KKK"));
+    }
 }
