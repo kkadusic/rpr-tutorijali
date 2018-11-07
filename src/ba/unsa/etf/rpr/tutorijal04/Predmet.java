7c1cd22913bb5b847bd31328d7ba79269b4ff165
kkadusic <kkadusic1@etf.unsa.ba>
2018-11-07 21:26:14 +0100

    Dodao atribute klase

--- src/ba/unsa/etf/rpr/tutorijal04/Predmet.java
+++ src/ba/unsa/etf/rpr/tutorijal04/Predmet.java
@@ -2,5 +2,11 @@ package ba.unsa.etf.rpr.tutorijal04;
 import java.util.*;
 
 public class Predmet {
-
+    private String nazivPredmeta;
+    private int sifraPredmeta;
+    private String imeProfesora;
+    private int ECTS;
+    private int kapacitet;
+    private int brojStudenata;
+    private Student[] Studenti;
 }
