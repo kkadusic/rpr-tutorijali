344e13a028bc4f5e3f8f9e24239d3a249e370adb
kkadusic <kkadusic1@etf.unsa.ba>
2018-11-07 21:41:09 +0100

    Dodao atribute, getere i setere

--- src/ba/unsa/etf/rpr/tutorijal04/PodaciOSemestru.java
+++ src/ba/unsa/etf/rpr/tutorijal04/PodaciOSemestru.java
@@ -3,5 +3,26 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class PodaciOSemestru {
+    private int odsjekID;
+    private Date datumUpisa;
+    private Date datumOvjere;
+    private int brojSemestra;
+    private ArrayList<Predmet> predmetiKojePohadja;
 
+    public PodaciOSemestru(Date datum_upisa, int broj_semestra) {
+        this.datumUpisa = datum_upisa;
+        this.brojSemestra = broj_semestra;
+    }
+
+    public ArrayList<Predmet> getPredmetiKojePohadja() {
+        return predmetiKojePohadja;
+    }
+
+    public int getOdsjekID() {
+        return odsjekID;
+    }
+
+    public void setOdsjekID(int broj) {
+        this.odsjekID = broj;
+    }
 }
