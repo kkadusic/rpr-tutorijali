2eab9453982b0203c2da93efa65e6469e026673c
kkadusic <kkadusic1@etf.unsa.ba>
2018-11-07 21:27:18 +0100

    Dodao getere i setere

--- src/ba/unsa/etf/rpr/tutorijal04/Predmet.java
+++ src/ba/unsa/etf/rpr/tutorijal04/Predmet.java
@@ -9,4 +9,52 @@ public class Predmet {
     private int kapacitet;
     private int brojStudenata;
     private Student[] Studenti;
+
+    public String getImePredmeta() {
+        return nazivPredmeta;
+    }
+
+    public void setImePredmeta(String imePredmeta) {
+        this.nazivPredmeta = imePredmeta;
+    }
+
+    public int getPredmetID() {
+        return sifraPredmeta;
+    }
+
+    public void setPredmetID(int predmetID) {
+        this.sifraPredmeta = predmetID;
+    }
+
+    public String getImeProfesora() {
+        return imeProfesora;
+    }
+
+    public void setImeProfesora(String imeProfesora) {
+        this.imeProfesora = imeProfesora;
+    }
+
+    public int getECTS() {
+        return ECTS;
+    }
+
+    public void setECTS(int ECTS) {
+        this.ECTS = ECTS;
+    }
+
+    public int getMaxBrojStudenata() {
+        return kapacitet;
+    }
+
+    public void setMaxBrojStudenata(int maxBrojStudenata) {
+        this.kapacitet = maxBrojStudenata;
+    }
+
+    public int getBrojStudenata() {
+        return brojStudenata;
+    }
+
+    public void setBrojStudenata(int brojStudenata) {
+        this.brojStudenata = brojStudenata;
+    }
 }
