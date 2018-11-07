4f48f761e738b16365f4fdbee3c24a6f1552a909
kkadusic <kkadusic1@etf.unsa.ba>
2018-11-07 21:19:11 +0100

    Dodao i zavrsio klasu

--- src/ba/unsa/etf/rpr/tutorijal04/Odsjek.java
+++ src/ba/unsa/etf/rpr/tutorijal04/Odsjek.java
@@ -2,5 +2,22 @@ package ba.unsa.etf.rpr.tutorijal04;
 import java.util.ArrayList;
 
 public class Odsjek {
+    private String naziv;
+    private int odsjekID;
+    private int brojSemestara;
+    private ArrayList<Semestar> semestri;
 
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public int odsjekID() {
+        return odsjekID;
+    }
+
+    public Semestar DajSemestar(int broj) throws ArrayIndexOutOfBoundsException{
+        if (broj < brojSemestara)
+            return semestri.get(broj);
+        else throw new ArrayIndexOutOfBoundsException();
+    }
 }
