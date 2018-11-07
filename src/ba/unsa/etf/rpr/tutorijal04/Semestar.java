c4b215c70ba62560e8f6ccc79d1cca2715600ee1
kkadusic <kkadusic1@etf.unsa.ba>
2018-11-07 21:34:20 +0100

    Dodao atribute, getere i setere

--- src/ba/unsa/etf/rpr/tutorijal04/Semestar.java
+++ src/ba/unsa/etf/rpr/tutorijal04/Semestar.java
@@ -2,5 +2,17 @@ package ba.unsa.etf.rpr.tutorijal04;
 import java.util.ArrayList;
 
 public class Semestar {
+    private int SemestarID;
+    private int brojObaveznihPredmeta;
+    private int brojIzbornihPredmeta;
+    private ArrayList<Predmet> izborniPredmeti;
+    private ArrayList<Predmet> obavezniPredmeti;
 
+    public ArrayList<Predmet> getIzborni() {
+        return izborniPredmeti;
+    }
+
+    public ArrayList<Predmet> getObavezni() {
+        return obavezniPredmeti;
+    }
 }
