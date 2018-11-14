cd5667073a8c2d93f6676e6eaae0bd417e542772
kkadusic <kkadusic1@etf.unsa.ba>
2018-11-14 19:32:20 +0100

    Dodao tipke za brojeve

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -24,4 +24,49 @@ public class Controller {
     public Controller(){
         displej = new SimpleStringProperty("0");
     }
+
+    public void btn00(ActionEvent actionEvent) {
+        if(displej.get()!="0") displej.set(displej.get()+"0");
+    }
+
+    public void btn11(ActionEvent actionEvent) {
+        if(displej.get()=="0"|| operacija.get()=="=") displej.set("");
+        displej.set(displej.get()+"1");
+    }
+
+    public void btn22(ActionEvent actionEvent) {
+        if(displej.get()=="0"|| operacija.get()=="=") displej.set("");
+        displej.set(displej.get()+"2");
+    }
+
+    public void btn33(ActionEvent actionEvent) {
+        if(displej.get()=="0"|| operacija.get()=="=") displej.set("");
+        displej.set(displej.get()+"3");
+    }
+
+    public void btn44(ActionEvent actionEvent) {
+        if(displej.get()=="0"|| operacija.get()=="=") displej.set("");
+        displej.set(displej.get()+"4");
+    }
+
+    public void btn66(ActionEvent actionEvent) {
+        if(displej.get()=="0"|| operacija.get()=="=") displej.set("");
+        displej.set(displej.get()+"6");
+    }
+
+    public void btn77(ActionEvent actionEvent) {
+        if(displej.get()=="0"|| operacija.get()=="=") displej.set("");
+        displej.set(displej.get()+"7");
+
+    }
+
+    public void btn88(ActionEvent actionEvent) {
+        if(displej.get()=="0"|| operacija.get()=="=") displej.set("");
+        displej.set(displej.get()+"8");
+    }
+
+    public void btn99(ActionEvent actionEvent) {
+        if(displej.get()=="0"|| operacija.get()=="=") displej.set("");
+        displej.set(displej.get()+"9");
+    }
 }
