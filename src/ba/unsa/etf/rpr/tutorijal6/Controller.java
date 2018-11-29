7d5da397fb625fd20be3218621813caf7090dd50
kkadusic <kkadusic1@etf.unsa.ba>
2018-11-29 01:08:37 +0100

    Dodao atribute

---
+++ src/ba/unsa/etf/rpr/tutorijal6/Controller.java
@@ -0,0 +1,33 @@
+package ba.unsa.etf.rpr.tutorijal6;
+
+import java.util.*;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class Controller {
+
+    public ComboBox mjesto;
+    public ComboBox status;
+    public ComboBox godina;
+    public ComboBox smjer;
+    public CheckBox pripadnost;
+    public TextField ime;
+    public TextField prezime;
+    public TextField index;
+    public TextField jmbg;
+    public TextField datum;
+    public TextField email;
+    public TextField adresa;
+    public TextField telefon;
+    private boolean imeValidno;
+    private boolean prezimeValidno;
+    private boolean indeksValidan;
+    private boolean jmbgValidno;
+    private boolean datumValidno;
+    private boolean emailValidno;
+
+
+
+}
