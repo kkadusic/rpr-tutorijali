a2450f6887ef1da9f755d7f994b44e538e66d78d
kkadusic <kkadusic1@etf.unsa.ba>
2018-12-05 22:24:05 +0100

    Popunio klasu

--- src/ba/unsa/etf/rpr/tutorijal7/UN.java
+++ src/ba/unsa/etf/rpr/tutorijal7/UN.java
@@ -1,6 +1,23 @@
 package ba.unsa.etf.rpr.tutorijal7;
+
 import java.util.*;
 
 public class UN {
+    private ArrayList<Drzava> drzave;
 
+    public UN() {
+        drzave = new ArrayList<>();
+    }
+
+    public UN(ArrayList<Drzava> drzave) {
+        this.drzave = drzave;
+    }
+
+    public ArrayList<Drzava> getDrzave() {
+        return drzave;
+    }
+
+    public void setDrzave(ArrayList<Drzava> drzave) {
+        this.drzave = drzave;
+    }
 }
