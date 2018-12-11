4629720b9d1479357599211a319c80e607a83f78
kkadusic <kkadusic1@etf.unsa.ba>
2018-12-11 17:01:23 +0100

    Dodao funkciju ucitajXml

--- src/ba/unsa/etf/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/etf/rpr/tutorijal7/Tutorijal.java
@@ -14,7 +14,11 @@ import javax.xml.parsers.DocumentBuilderFactory;
 public class Tutorijal {
 
     public static void main(String[] args) {
-        // write your code here
+        UN un = ucitajXml(null);
+
+        for(Drzava d : un.getDrzave()) {
+            System.out.println(d);
+        }
     }
 
     public static ArrayList<Grad> ucitajGradove() {
@@ -43,6 +47,45 @@ public class Tutorijal {
         }
 
         return niz;
+    }
+
+    public static UN ucitajXml(ArrayList<Grad> gradovi) {
+        Document xmldoc = null;
+        try {
+            DocumentBuilder docReader = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            xmldoc = docReader.parse(new File("drzave.xml"));
+        } catch (Exception e) {
+            System.out.println("Greska: drzave.xml nije ispravan XML dokument");
+        }
+
+        UN un = new UN();
+        ArrayList<Drzava> drzave = new ArrayList<>();
+
+        NodeList drzaveXml = xmldoc.getElementsByTagName("drzava");
+
+        for(int i = 0; i < drzaveXml.getLength(); i++) {
+            Node drzavaNode = drzaveXml.item(i);
+
+            if(drzavaNode instanceof Element) {
+                Element drzavaEl = (Element)drzavaNode;
+
+                int stanovnika = Integer.parseInt(drzavaEl.getAttribute("stanovnika"));
+                String naziv = drzavaEl.getElementsByTagName("naziv").item(0).getTextContent();
+
+                Element gGradXml = (Element)drzavaEl.getElementsByTagName("glavnigrad").item(0);
+                int gStanovnika = Integer.parseInt(gGradXml.getAttribute("stanovnika"));
+                String nazivGrada = gGradXml.getTextContent().trim();
+
+                Element povrsinaXml = (Element)drzavaEl.getElementsByTagName("povrsina").item(0);
+                String jedinica = povrsinaXml.getAttribute("jedinica");
+                double povrsina = Double.parseDouble(drzavaEl.getElementsByTagName("povrsina").item(0).getTextContent());
+
+                Grad glavniGrad = new Grad(nazivGrada, gStanovnika, null);
+                drzave.add(new Drzava(naziv, stanovnika, povrsina, jedinica, glavniGrad));
+            }
+        }
+        un.setDrzave(drzave);
+        return un;
     }
 
     public static void zapisiXml(UN un) {
