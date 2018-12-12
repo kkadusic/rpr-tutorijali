56e017aa329eb379ea6bc1da592cce23d403cc24
kkadusic <kkadusic1@etf.unsa.ba>
2018-12-12 18:02:15 +0100

    Sredjen main

--- src/ba/unsa/etf/rpr/tutorijal8/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal8/Main.java
@@ -4,17 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Pretraga datoteka");
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         primaryStage.show();
+        primaryStage.setResizable(true);
     }
+
 
     public static void main(String[] args) {
         launch(args);
