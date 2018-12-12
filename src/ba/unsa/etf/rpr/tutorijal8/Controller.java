6312d14283488ef54ac5145fa6c1a0fc7cec83e8
kkadusic <kkadusic1@etf.unsa.ba>
2018-12-12 18:06:55 +0100

    Dodani atributi i osnovne metode

--- src/ba/unsa/etf/rpr/tutorijal8/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal8/Controller.java
@@ -1,5 +1,51 @@
 package ba.unsa.etf.rpr.tutorijal8;
 
+import javafx.application.Platform;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.ProgressIndicator;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Controller {
+    public SimpleStringProperty text;
+    public SimpleListProperty<String> lista;
+    public TextField searchField;
+    public Button searchBtn;
+    public ListView list;
+    public List<String> result1;
+    public Button stopButton;
+    public ProgressIndicator progressIndicator;
+    public boolean prekidanje = false;
+
+    private Thread thread1, thread2;
+
+    public Controller() {
+        text = new SimpleStringProperty("");
+        lista = new SimpleListProperty<>();
+        result1 = Collections.synchronizedList(new ArrayList<>());
+        progressIndicator = new ProgressIndicator(0);
+    }
+
+    @FXML
+    public void initialize() {
+        searchField.textProperty().bindBidirectional(text);
+        list.itemsProperty().bindBidirectional(lista);
+        lista.set(FXCollections.observableArrayList(result1));
+        stopButton.setDisable(true);
+        prekidanje = false;
+        progressIndicator.setProgress(0);
+    }
+
+
 
 }
