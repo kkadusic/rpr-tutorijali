f60aa6929e2e68a98f48d5dde854e092ffe82d12
kkadusic <kkadusic1@etf.unsa.ba>
2018-12-19 00:53:42 +0100

    Zavrsena DTO klasa

--- src/ba/unsa/etf/rpr/Drzava.java
+++ src/ba/unsa/etf/rpr/Drzava.java
@@ -1,12 +1,56 @@
 package ba.unsa.etf.rpr;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Drzava {
+    private SimpleIntegerProperty id;
+    private SimpleStringProperty naziv;
+    private Grad glavniGrad;
 
-    public void setNaziv(String bosna_i_hercegovina) {
-
+    public Drzava() {
+        id = new SimpleIntegerProperty(0);
+        naziv = new SimpleStringProperty("");
+        glavniGrad = new Grad();
     }
 
-    public void setGlavniGrad(Grad sarajevo) {
+    public Drzava(SimpleIntegerProperty id, SimpleStringProperty naziv, Grad glavniGrad) {
+        this.id = id;
+        this.naziv = naziv;
+        this.glavniGrad = glavniGrad;
+    }
 
+    public int getId() {
+        return id.get();
+    }
+
+    public SimpleIntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+
+    public String getNaziv() {
+        return naziv.get();
+    }
+
+    public SimpleStringProperty nazivProperty() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv.set(naziv);
+    }
+
+
+    public Grad getGlavniGrad() {
+        return glavniGrad;
+    }
+
+    public void setGlavniGrad(Grad glavniGrad) {
+        this.glavniGrad = glavniGrad;
     }
 }
--- src/ba/unsa/etf/rpr/Grad.java
+++ src/ba/unsa/etf/rpr/Grad.java
@@ -1,27 +1,71 @@
 package ba.unsa.etf.rpr;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Grad {
+    private SimpleIntegerProperty id, brojStanovnika;
+    private SimpleStringProperty naziv;
+    private Drzava drzava;
 
-    public String getNaziv() {
-        return null;
+    public Grad() {
+        id = new SimpleIntegerProperty(0);
+        brojStanovnika = new SimpleIntegerProperty(0);
+        naziv = new SimpleStringProperty("");
+        drzava = null;
     }
 
-    public Grad getDrzava() {
-        return null;
+    public Grad(SimpleIntegerProperty id, SimpleIntegerProperty brojStanovnika, SimpleStringProperty naziv, Drzava drzava) {
+        this.id = id;
+        this.brojStanovnika = brojStanovnika;
+        this.naziv = naziv;
+        this.drzava = drzava;
     }
 
-    public void setNaziv(String marseille) {
-
+    public int getId() {
+        return id.get();
     }
 
-    public void setBrojStanovnika(int i) {
+    public SimpleIntegerProperty idProperty() {
+        return id;
     }
 
-    public void setDrzava(Drzava francuska) {
-
+    public void setId(int id) {
+        this.id.set(id);
     }
+
 
     public int getBrojStanovnika() {
-        return 0;
+        return brojStanovnika.get();
+    }
+
+    public SimpleIntegerProperty brojStanovnikaProperty() {
+        return brojStanovnika;
+    }
+
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika.set(brojStanovnika);
+    }
+
+
+    public String getNaziv() {
+        return naziv.get();
+    }
+
+    public SimpleStringProperty nazivProperty() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv.set(naziv);
+    }
+
+
+    public Drzava getDrzava() {
+        return drzava;
+    }
+
+    public void setDrzava(Drzava drzava) {
+        this.drzava = drzava;
     }
 }
