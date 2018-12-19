a9d7299db2ca189b0a18df3fa92b38c962981d6d
kkadusic <kkadusic1@etf.unsa.ba>
2018-12-19 21:30:50 +0100

    Zavrsena klasa, ZAVRSENA priprema

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -5,16 +5,28 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static String ispisiGradove(){
+        ArrayList<Grad> gradovi = GeografijaDAO.getInstance().gradovi();
+        String s = "";
+        for (Grad grad : gradovi)
+            s += grad.getNaziv() + " (" + grad.getDrzava().getNaziv() + ")" + " - " + grad.getBrojStanovnika() + "\n";
+        return s;
+    }
+
+    public static void glavniGrad(){
+        System.out.println("Unesite ime drzave: ");
+        Scanner unos = new Scanner (System.in);
+        String drzava = unos.nextLine();
+        Grad grad = GeografijaDAO.getInstance().glavniGrad(drzava);
+        if (grad != null)
+            System.out.println("Glavni grad drzave " + drzava + " je " + grad.getNaziv());
+        else
+            System.out.println("Nepostojeca drzava");
+    }
+
     public static void main(String[] args) {
         System.out.println("Gradovi su:\n" + ispisiGradove());
         glavniGrad();
-    }
 
-    private static void glavniGrad() {
-
-    }
-
-    public static String ispisiGradove() {
-        return null;
     }
 }
