f9eaed6360551209e0aa8e4d26df645cc0d5ab9a
kkadusic <kkadusic1@etf.unsa.ba>
2018-12-27 03:23:23 +0100

    Zavrsen main, zavrsen cijeli tutorijal

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,9 +1,23 @@
 package ba.unsa.etf.rpr;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
-public class Main {
+public class Main extends Application {
+
+    private static Stage stage;
+    public static Stage getStage() {
+        return stage;
+    }
 
     public static String ispisiGradove(){
         ArrayList<Grad> gradovi = GeografijaDAO.getInstance().gradovi();
@@ -25,8 +39,26 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Gradovi su:\n" + ispisiGradove());
-        glavniGrad();
+        //System.out.println(ispisiGradove());
+        //glavniGrad();
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        stage = primaryStage;
+        ResourceBundle bundle = ResourceBundle.getBundle("Translation");
+        Parent root = FXMLLoader.load(getClass().getResource("glavna.fxml"), bundle);
+        stage.setTitle("Drzave");
+        stage.setScene(new Scene(root, 640, 400));
+        stage.show();
+    }
+
+    public static void loadView(Locale locale) throws IOException {
+        ResourceBundle bundle = ResourceBundle.getBundle("Translation", locale);
+        Parent root = FXMLLoader.load(Main.class.getResource("glavna.fxml"), bundle);
+        stage.setTitle("Drzave");
+        stage.setScene(new Scene(root, 640, 400));
+        stage.show();
     }
 }
