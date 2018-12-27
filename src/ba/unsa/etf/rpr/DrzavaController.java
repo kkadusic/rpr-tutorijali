bba9e916cc94fa366377829a84b3804f1465cbaf
kkadusic <kkadusic1@etf.unsa.ba>
2018-12-27 03:15:11 +0100

    Dodana kontroler klasa i fxml za novi izvjestaj

---
+++ src/ba/unsa/etf/rpr/DrzavaController.java
@@ -0,0 +1,40 @@
+package ba.unsa.etf.rpr;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import net.sf.jasperreports.engine.JRException;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class DrzavaController implements Initializable {
+    public ComboBox<String> drzavaCombo;
+    private ObservableList<String> listaDrzava = FXCollections.observableArrayList();
+    ArrayList<Drzava> drzave;
+
+    public DrzavaController() {}
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        drzave = GeografijaDAO.getInstance().drzave();
+        System.out.println(drzave);
+        for (Drzava d: drzave) {
+            listaDrzava.add(d.getNaziv());
+        }
+        drzavaCombo.setItems(listaDrzava);
+    }
+
+    public void kreirajIzvjestaj(ActionEvent actionEvent) {
+        GradoviReport gradoviReport = new GradoviReport();
+        try {
+            gradoviReport.showReportDrzava(GeografijaDAO.getConn(), drzavaCombo.getValue());
+        }
+        catch (JRException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
