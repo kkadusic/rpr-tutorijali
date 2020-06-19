21eea65c0acc8af185856b2a49882d6140c51b72
Kerim Kadušić <44180058+kkadusic@users.noreply.github.com>
2020-06-19 21:02:48 +0200

    Tutorijal 8

---
+++ rpr-t8/src/ba/unsa/etf/rpr/tutorijal8/Controller.java
@@ -0,0 +1,111 @@
+package ba.unsa.etf.rpr.tutorijal8;
+
+import javafx.application.Platform;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.ProgressIndicator;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Controller {
+    public SimpleStringProperty text;
+    public SimpleListProperty<String> lista;
+    public TextField searchField;
+    public Button searchBtn;
+    public ListView list;
+    public List<String> result1;
+    public Button stopButton;
+    public ProgressIndicator progressIndicator;
+    public boolean prekidanje = false;
+
+    private Thread thread1, thread2;
+
+    public Controller() {
+        text = new SimpleStringProperty("");
+        lista = new SimpleListProperty<>();
+        result1 = Collections.synchronizedList(new ArrayList<>());
+        progressIndicator = new ProgressIndicator(0);
+    }
+
+    @FXML
+    public void initialize() {
+        searchField.textProperty().bindBidirectional(text);
+        list.itemsProperty().bindBidirectional(lista);
+        lista.set(FXCollections.observableArrayList(result1));
+        stopButton.setDisable(true);
+        prekidanje = false;
+        progressIndicator.setProgress(0);
+    }
+
+    public void nadjiFajlove(File f) {
+        if (prekidanje) return;
+        try {
+            File[] files = f.listFiles();
+            if (files == null) return;
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    nadjiFajlove(file);
+                } else {
+                    if (file.getCanonicalPath().toLowerCase().contains(searchField.getText().toLowerCase())) {
+                        String result = file.getCanonicalPath();
+                        Platform.runLater(() -> {
+                            list.getItems().add(result);
+                        });
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void clickOnSearchBtn(ActionEvent actionEvent) {
+        Runnable r = () -> nadjiFajlove(new File(System.getProperty("user.home")));
+        Thread thread = new Thread(r);
+        thread.start();
+        initialize();
+        Runnable r1 = () -> {
+            searchBtn.setDisable(true);
+            searchField.setDisable(true);
+            stopButton.setDisable(false);
+            nadjiFajlove(new File(System.getProperty("user.home")));
+        };
+        Runnable r2 = () -> {
+            for (int i = 1; i <= 1000; i++) {
+                if (prekidanje) {
+                    progressIndicator.setProgress(0);
+                    break;
+                }
+                progressIndicator.setProgress(i / 1000.0);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread1 = new Thread(r1);
+        thread2 = new Thread(r2);
+        thread1.start();
+        thread2.start();
+    }
+
+    public void clickOnStopButton(ActionEvent actionEvent) {
+        if (thread1!= null || thread2!=null) {
+            prekidanje = true;
+            searchBtn.setDisable(false);
+            searchField.setDisable(false);
+            stopButton.setDisable(true);
+        }
+    }
+
+}
---
+++ rpr-t8/src/ba/unsa/etf/rpr/tutorijal8/Main.java
@@ -0,0 +1,27 @@
+package ba.unsa.etf.rpr.tutorijal8;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Pretraga datoteka");
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.show();
+        primaryStage.setResizable(true);
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
